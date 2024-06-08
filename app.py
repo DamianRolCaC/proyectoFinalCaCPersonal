3ae1547d792b0db2aec297d773b67c1b8a9acddb
DamianRolCaC <damian.rol@bue.edu.ar>
2024-06-08 00:27:34 -0300

    Subida Proyecto Final CaC Personal

--- app.py
+++ app.py
@@ -118,15 +118,15 @@ class Catalogo:
 # Cuerpo del programa
 #--------------------------------------------------------------------
 # Crear una instancia de la clase Catalogo
-catalogo = Catalogo(host='localhost', user='root', password='root', database='miapp')
-#catalogo = Catalogo(host='USUARIO.mysql.pythonanywhere-services.com', user='USUARIO', password='CLAVE', database='USUARIO$miapp')
+#catalogo = Catalogo(host='localhost', user='root', password='root', database='miapp')
+catalogo = Catalogo(host='damrol.mysql.pythonanywhere-services.com', user='damrol', password='CodoaCodo2024', database='damrol$miapp')
 
 
 # Carpeta para guardar las imagenes.
-RUTA_DESTINO = './static/imagenes/'
+#RUTA_DESTINO = './static/imagenes/'
 
 #Al subir al servidor, deberá utilizarse la siguiente ruta. USUARIO debe ser reemplazado por el nombre de usuario de Pythonanywhere
-#RUTA_DESTINO = '/home/USUARIO/mysite/static/imagenes'
+RUTA_DESTINO = '/home/damrol/mysite/static/imagenes'
 
 
 #--------------------------------------------------------------------
